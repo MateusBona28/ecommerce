97f0f76588b2772631f7b276e32cf3fa3f40bf02
unknown <mateusbona28@gmail.com>
2022-10-24 15:36:34 -0300

    feat: funções genéricas para filtrar dados de rotas finalizadas

---
+++ src/utils/getGenerics.ts
@@ -0,0 +1,41 @@
+import AppDataSource from "../data-source"
+import { User } from "../entities/user.entity"
+import AppError from "../errors/AppError"
+
+
+export const getByIdGeneric = async (model: any, instanceId:any , callback?: any) => {
+
+    const modelRepository = AppDataSource.getRepository(model)
+
+    const instance = modelRepository.findOneBy({
+        id: instanceId
+    })
+
+    if (callback) {
+        callback(instance)
+    }
+
+    return instance
+
+}
+
+
+export const getObjectOr404 = async (model: any, objectKey: any, objectKeyValue: any ) => {
+
+    const modelRepository = AppDataSource.getRepository(model)
+
+    const instances = await modelRepository.find()
+
+    const instance = instances.find(objectInstance => objectInstance[`${objectKey}`] === objectKeyValue)
+
+    if (!instance && model === User) {
+        throw new AppError("invalid username", 404);
+    }
+
+    if (!instance) {
+        throw new AppError("Not found", 404);
+    }
+
+    return instance
+
+}
